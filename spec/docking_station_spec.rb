848b015e748089b87a4d5b33f55723adf2e62f73
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-05-29 16:29:32 +0100

    DockingStationSpec One Line

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,11 @@
 require 'docking_station'
 
-describe DockingStation do
-  it 'responds to release bike method' do
-    expect(subject).to respond_to :release_bike
-  end
-#  it 'releases the bike' do
-#    expect(release_bike).to eq 'true'
+#describe DockingStation do
+#  it 'responds to release bike method' do
+#    expect(subject).to respond_to :release_bike
 #  end
+#end
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 end
